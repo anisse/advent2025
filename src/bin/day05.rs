57dcbb274088bb85490ee26fac52a46c3feba79c
Anisse Astier <anisse@astier.eu>
2025-12-05 09:32:14 +0100

    day05: part 2

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -36,14 +36,41 @@ where
         .count()
 }
 
-fn part2<I>(ranges: &[[u64; 2]], things: I) -> usize
+fn part2<I>(ranges: &[[u64; 2]], _: I) -> u64
 where
     I: Iterator<Item = ParsedItem>,
 {
-    for _ in things {
-        todo!()
+    let mut ranges_merged = ranges.to_vec();
+    let mut to_merge = true;
+    while to_merge {
+        to_merge = false;
+        'outer: for i in 0..(ranges_merged.len() - 1) {
+            for j in (i + 1)..ranges_merged.len() {
+                //println!("{:?} vs {:?}", ranges_merged[i], ranges_merged[j]);
+                if let Some(new) = merge_on_overlap(&ranges_merged[i], &ranges_merged[j]) {
+                    //println!("merging");
+                    ranges_merged.remove(j);
+                    ranges_merged[i] = new;
+                    to_merge = true;
+                    break 'outer;
+                }
+            }
+        }
     }
-    42
+    ranges_merged
+        .iter()
+        .map(|[start, end]| end - start + 1)
+        .sum()
+}
+
+fn merge_on_overlap(r1: &[u64; 2], r2: &[u64; 2]) -> Option<[u64; 2]> {
+    let ra1 = r1[0]..=r1[1];
+    let ra2 = r2[0]..=r2[1];
+    if ra1.contains(&r2[0]) || ra1.contains(&r2[1]) || ra2.contains(&r1[0]) || ra2.contains(&r1[1])
+    {
+        return Some([r1[0].min(r2[0]), r1[1].max(r2[1])]);
+    }
+    None
 }
 
 #[test]
