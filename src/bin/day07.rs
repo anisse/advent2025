efafdadcec4e4dfb53be1b135c1fbfeef20cddd3
Anisse Astier <anisse@astier.eu>
2025-12-07 06:57:48 +0100

    day07: part 2

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use advent2025::*;
 fn main() {
     let map = grid(input!());
@@ -26,9 +28,7 @@ fn part1(map: MapRef) -> usize {
             split += 1;
         }
     });
-    print_map(&map);
-    // count beams on last line
-    //map[map.len() - 1].iter().filter(|c| **c == b'|').count()
+    //print_map(&map);
     split
 }
 
@@ -42,9 +42,83 @@ fn add_beam(map: MapRefMut, mut pos: Coord) {
         pos = pos + (0, 1);
     }
 }
-
+type Cache = HashMap<Coord, usize>;
 fn part2(map: MapRef) -> usize {
-    42
+    let start = find_first(map, b'S');
+    let mut cache = Cache::new();
+    beam(&mut cache, start + (0, 1), map)
+    /*
+    let splitters = map
+        .iter()
+        .enumerate()
+        .map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .filter(|(_, c)| **c == b'^')
+                .map(|(x, _)| Coord::from((x, y)))
+                .collect::<Vec<_>>()
+        })
+        .filter(|line| !line.is_empty())
+        .collect::<Vec<_>>();
+    map[start.y() + 1][start.x()] = b'|';
+
+    splitters
+        .into_iter()
+        .map(|line| {
+            line.into_iter()
+                .map(|pos| {
+                    let up = *pos + (0, -1);
+                    if map[up.y()][up.x()] == b'|' {
+                        let left = *pos + (-1, 0);
+                        let right = *pos + (1, 0);
+                        if left.valid_for(&map) && {
+                        }
+                        add_beam(&mut map, left);
+                        add_beam(&mut map, right);
+                        print_map(&map);
+                        return true;
+                    }
+                    false
+                })
+                .count()
+        })
+        .inspect(|x| println!("{x:?} splitters"))
+        .product()
+        */
+}
+
+fn beam(cache: &mut Cache, mut pos: Coord, map: MapRef) -> usize {
+    if let Some(x) = cache.get(&pos) {
+        return *x;
+    }
+    while pos.valid_for(map) {
+        if map[pos.y()][pos.x()] != b'.' {
+            break;
+        }
+        pos = pos + (0, 1);
+    }
+    if let Some(x) = cache.get(&pos) {
+        return *x;
+    }
+    let mut count = 1;
+    if pos.valid_for(map) && map[pos.y()][pos.x()] == b'^' {
+        println!("{pos:?}");
+        let left = pos + (-1, 0);
+        let right = pos + (1, 0);
+        let count_left = if left.valid_for(map) {
+            beam(cache, left, map)
+        } else {
+            0
+        };
+        let count_right = if right.valid_for(map) {
+            beam(cache, right, map)
+        } else {
+            0
+        };
+        count = count_left + count_right;
+    }
+    cache.insert(pos, count);
+    count
 }
 
 #[test]
@@ -52,8 +126,8 @@ fn test() {
     let map = grid(sample!());
     //part 1
     let res = part1(&map);
-    assert_eq!(res, 42);
+    assert_eq!(res, 21);
     //part 2
-    let res = grid(&map);
-    assert_eq!(res, 42);
+    let res = part2(&map);
+    assert_eq!(res, 40);
 }
