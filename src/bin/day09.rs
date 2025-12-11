52ac86484e3ef7bcb2b037c53853f6d3ab7fc47e
Anisse Astier <anisse@astier.eu>
2025-12-11 08:30:25 +0100

    day09: cleanup

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use advent2025::*;
 fn main() {
     let things = parse(input!());
@@ -100,60 +98,6 @@ fn sorted_seg(s: Segment) -> Segment {
 fn segment_in_rect(s: &Segment, r: &Segment) -> bool {
     s.1.0 > r.0.0 && s.0.0 < r.1.0 && s.1.1 > r.0.1 && s.0.1 < r.1.1
 }
-
-/*
-fn intersect(s1: Segment, s2: Segment) -> bool {
-    let res = proper_inter(s1, s2)
-        || on_segment(s2, s1.0)
-        || on_segment(s2, s1.1)
-        || on_segment(s1, s2.0)
-        || on_segment(s1, s2.1);
-    //println!("{res} intersect: {s1:?} vs {s2:?}");
-    res
-    /*
-    proper_inter(
-        ((s1.0.0, s1.0.1 + 1), (s1.1.0, s1.1.1 + 1)),
-        ((s2.0.0, s2.0.1 + 1), (s2.1.0, s2.1.1 + 1)),
-    )
-    */
-}
-
-// From Victor Lecomte cp-geo handbook
-fn cross(a: Point, b: Point) -> i64 {
-    a.0 * b.1 - a.1 * b.0
-}
-fn sub(a: Point, b: Point) -> Point {
-    (a.0 - b.0, a.1 - b.1)
-}
-
-fn orient(a: Point, b: Point, c: Point) -> i64 {
-    cross(sub(b, a), sub(c, a))
-}
-
-fn proper_inter(s1: Segment, s2: Segment) -> bool {
-    let a = s1.0;
-    let b = s1.1;
-    let c = s2.0;
-    let d = s2.1;
-
-    let oa = orient(c, d, a);
-    let ob = orient(c, d, b);
-    let oc = orient(a, b, c);
-    let od = orient(a, b, d);
-    //println!("{oa} {ob} - {oc} {od}");
-    // Proper intersection exists iff opposite signs
-    oa.signum() * ob.signum() < 0 && oc.signum() * od.signum() < 0
-}
-fn dot(a: Point, b: Point) -> i64 {
-    a.0 * b.0 + a.1 * b.1
-}
-fn in_disk(s: Segment, p: Point) -> bool {
-    dot(sub(s.0, p), sub(s.1, p)) <= 0
-}
-fn on_segment(s: Segment, p: Point) -> bool {
-    orient(s.0, s.1, p) == 0 && in_disk(s, p)
-}
-*/
 
 #[test]
 fn test() {
